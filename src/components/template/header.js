70a9d4d7042fe2cb80977b8efffeef3dc5852307
Akash deep <aakashdeep@mobcoder.com>
2021-04-24 22:30:21 +0530

    navbar togglar

--- src/components/template/header.js
+++ src/components/template/header.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Navbar, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link';
 
 const Header = () => {
@@ -11,7 +11,7 @@ const Header = () => {
     return (
         <header className="header-container">
             <div className="page-container ">
-                <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                {/* <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
                     <div className="container">
                         <a class="navbar-brand" href="#">Akash Deep</a>
                         <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,7 +33,30 @@ const Header = () => {
 
                         </div>
                     </div>
-                </nav>
+                </nav> */}
+                <Navbar expand="md" className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                    <div className="container">
+                        <Link to="/" exact class="navbar-brand">
+                            Akash Deep
+                        </Link>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" >
+                            <i className="fas fa-bars"></i>
+                        </Navbar.Toggle>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ml-auto">
+                                <li class="nav-item active">
+                                    <NavLink to="/" exact className="nav-link">Home</NavLink>
+                                </li>
+                                <li class="nav-item">
+                                    <HashLink smooth to="#projects" exact className="nav-link">Project</HashLink>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="https://drive.google.com/file/d/1m0RxWeKyhkiLSmg7d72k5mOXbGZcE1Uz/view" className="nav-link" target="_blank" >Resume</a>
+                                </li>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </div>
+                </Navbar>
             </div >
         </header >
     )
